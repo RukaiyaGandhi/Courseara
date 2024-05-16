8ace48894b00734208645db21fa706a833f00822
RukaiyaGandhi <166583839+RukaiyaGandhi@users.noreply.github.com>
2024-05-16 12:44:30 +0530

    Update script.js

--- Module 4/script.js
+++ Module 4/script.js
@@ -5,7 +5,7 @@
     for (var i = 0; i < names.length; i++) {
       var firstLetter = names[i].charAt(0).toLowerCase();
     
-      if (firstLetter === 'a') {
+      if (firstLetter === 'j') {
         byeSpeaker(names[i]);
       }
       else {
